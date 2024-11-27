09f78d56c71973cb58a96c16e725e479a7855d1a
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-27 18:01:30 +0900

    feat: post 엔티티 생성

---
+++ backend/src/main/java/cotato/backend/domains/post/entity/Post.java
@@ -0,0 +1,39 @@
+package cotato.backend.domains.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Post {
+	@Id
+	@Column(name = "post_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private int views = 0;
+
+
+
+}
